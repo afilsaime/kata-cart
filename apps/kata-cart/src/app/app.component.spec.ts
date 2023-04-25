eb82a0efd646931b777ae79dc944ed4a4291a7f3
afilsaime <shadow_kungfu@hotmail.fr>
2023-04-25 15:19:34 +0200

    fix app component unit tests

--- apps/kata-cart/src/app/app.component.spec.ts
+++ apps/kata-cart/src/app/app.component.spec.ts
@@ -1,27 +1,24 @@
+import {
+  Spectator,
+  SpectatorRouting,
+  createRoutingFactory,
+} from '@ngneat/spectator/jest';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
-    }).compileComponents();
+  let spectator: SpectatorRouting<AppComponent>;
+
+  const createComponent = createRoutingFactory({ component: AppComponent });
+
+  beforeEach(() => {
+    spectator = createComponent();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome kata-cart'
+  it('should have the right title', () => {
+    expect(spectator.query('[data-test=app-name]')?.textContent).toEqual(
+      'Kata Panier'
     );
-  });
-
-  it(`should have as title 'kata-cart'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('kata-cart');
   });
 });
